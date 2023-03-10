151246edad438a7b19ea1fb1965ca1df7e8157e0
Emma <cemma313@gmail.com>
2023-03-10 13:07:27 -0700

    moved code into routes and reformatted server.js

--- Develop/public/assets/js/routes.js
+++ Develop/public/assets/js/routes.js
@@ -2,6 +2,12 @@
 app.get("/api/notes", (req, res) => {
     res.json(noteText);
 });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
+  app.get('/api/notes', (req, res) => res.json(noteText));
+  
 
 //POST
 app.post("/api/notes", (req, res)=> {
--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,21 @@
 //Required
 const fs = require("fs");
-const termData = require("./db/db.json");
 const express = require("express");
-
-const PORT = 3001;
-
+const routes = require('./public/assets/js/routes');
 const app = express();
 
-//middle
+const PORT = process.env.PORT || 3001;
+
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/api/notes', (req, res) => res.json(noteText));
+app.use('/api', routes);
 
 //PORT listening to server
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
-
 
 
 module.exports = app
