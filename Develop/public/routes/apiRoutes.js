6768ec9c806f6ab03ce3163b646d56bc5bfcae65
Emma <cemma313@gmail.com>
2023-03-12 10:19:43 -0600

    all get routes working, for notes page

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -1,21 +1,29 @@
-//Required
+Required;
 const fs = require("fs");
 const router = require("express").Router();
+const db = require("./Develop/db/db.json");
+const path = require("path");
 
 //GET request
-router.get("api/notes", (req, res) => {
+router.get("/api/notes", (req, res) => {
   fs.readFileSync("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
     res.json(JSON.parse(data));
   });
 });
 
-// //
-// // //POST
-// // app.post("/api/notes", (req, res)=> {
-// // readFile("db.json", "utf8")
-// // const newNote = req.body;
-// // });
-// // res.json(data)
+// POST
+// router.post('/api/notes', (req, res) => {
+//   fs.readFile("./db/db.json", "utf8", (err, data) => {
+//     if (err) throw err;
+//     const getNote = JSON.parse(data);
+//     getNote.push(req.body);
+//     fs.writeFile("./db.db.json", JSON.stringify(getNote), (err) => {
+//       if (err) return err;
+//       console.log('yay');
+//     })
+//   })
+// });
 
 // //DELETE
 
--- Develop/public/routes/htmlRoutes.js
+++ Develop/public/routes/htmlRoutes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const path = require("path");
 
 //GET notes html
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+router.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
 });
 //GET index html
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+router.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "Develop/public/index.html"));
 });
 
 module.exports = router;
