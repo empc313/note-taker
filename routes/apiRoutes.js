f241afd65c2f5ac7cff8ec022861563f324b1fb7
Emma <cemma313@gmail.com>
2023-03-13 13:51:08 -0600

    Can click on note and recall data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,33 +2,31 @@
 const fs = require("fs");
 const router = require("express").Router();
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
-
 
 //GET request
-router.get('/', (req, res) => {
-  fs.readFile('./db/db.json', "utf8", (err, data) => {
+router.get("/", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    console.log(JSON.parse(data))
+    console.log(JSON.parse(data));
     res.send(data);
   });
 });
 
 //POST
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const newNote = {
     title: req.body.title,
     text: req.body.text,
-    id: uuidv4(),
-  }
-  fs.readFile('./db/db.json', (err,data) => {
-    if(err) throw err;
+    id: req.body,
+  };
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
     const noteText = JSON.parse(data);
     noteText.push(newNote);
-    fs.writeFile('./db/db.json', JSON.stringify(noteText, null, 1), (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(noteText, null, 1), (err) => {
       if (err) throw err;
-        err ? console.log(err) : res.send(newNote);
-      })
+      err ? console.log(err) : res.send(newNote);
+    });
   });
 });
 
